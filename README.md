b39ea4fd46bb55c969bc2ef7b9b17f1d965aa087
Simon <simonvervisch@gmail.com>
2019-10-19 14:14:06 +0200

    improved

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Ascii Art
+
+## Basic Ascii art maker
+
+Plain and simple. Input a string. Output is in display. 
+
--- Write-ASCII-Art.rkt
+++ Write-ASCII-Art.rkt
@@ -1,27 +1,42 @@
-#lang racket
-
+#lang racket/base
 
 (define (write-in-ascii string)
+  
   (let ((line-length 40)
-        (space-length 3))
-    (define (display-char n char)
-      (when (> n 0)
-        (display char)
-        (display-char (- n 1) char)))
-    (define (display-line-with-string) 
-      (let* ((middle-char-length (+ (* 2 space-length)
-                                      (string-length string)))
-            (left-side-char-length (quotient (- line-length
-                                      middle-char-length) 2))
-            (right-side-char-length (- line-length left-side-char-length middle-char-length)))
-        (display-char left-side-char-length ";")
-        (display-char space-length " ")
-        (display string)
-        (display-char space-length " ")
-        (display-char right-side-char-length ";")))
-     (display-char line-length ";")
-     (newline)
-     (display-line-with-string)
-     (newline)
-     (display-char line-length ";")))
+        (space-length 3)
+        (string (string-upcase string)))
+    (cond ((< (string-length string) (- line-length 2))
+           (define (display-char n char)
+             (when (> n 0)
+               (display char)
+               (display-char (- n 1) char)))
+           (define (display-line-with-string)
+      
+             (let* ((middle-char-length (+ (* 2 space-length)
+                                           (string-length string)))
+                    (left-side-char-length (quotient (- line-length
+                                                        middle-char-length) 2))
+                    (right-side-char-length (- line-length left-side-char-length middle-char-length)))
+               (display-char left-side-char-length ";")
+               (display-char space-length " ")
+               (display string)
+               (display-char space-length " ")
+               (display-char right-side-char-length ";")))
+           (display-char line-length ";")
+           (newline)
+           (display-line-with-string)
+           (newline)
+           (display-char line-length ";"))
+          (else (display "string too long")))))
+
+
+(display "Insert 'stop' to finish")
+(newline)
+(let loop
+  ((x (symbol->string (read))))
+  (if (string=? x "stop")
+      (display "No more input")
+      (begin (write-in-ascii x)
+             (newline)
+             (loop (symbol->string (read))))))
 
